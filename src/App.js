f51942c672e877349a80f6e5263ad2d7d9a0e7d7
m19e <nexus.me7290@gmail.com>
2020-12-18 00:10:03 +0900

    Test bars spinner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <>
+        {[1, 2, 3, 4, 5, 6, 7].map((i) => (
+            <div style={{ display: "flex", width: "100px", height: "100px" }}>
+                <div id={"bars" + i}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        ))}
+    </>
+);
 
 export default App;
