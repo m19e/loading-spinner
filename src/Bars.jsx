97b388e3bcf13bec32f4c3fee9806f34423f10f0
m19e <nexus.me7290@gmail.com>
2020-12-18 00:40:55 +0900

    Add three bars test

---
+++ src/Bars.jsx
@@ -0,0 +1,34 @@
+import "./Bars.css";
+
+const styles = {
+    flexCenter: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    row: {
+        width: "100%",
+        height: "10%",
+    },
+};
+
+export default function Bars() {
+    return (
+        <div style={{ width: "100vw", height: "100vh" }}>
+            <div className="spinners" style={{ display: "flex", flex: 1, flexDirection: "column", height: "100%" }}>
+                <div className="row" style={{ ...styles.flexCenter, ...styles.row }}>
+                    <div className="spinner spinner-bounce-bottom"></div>
+                </div>
+                <div className="row" style={{ ...styles.flexCenter, ...styles.row }}>
+                    <div className="spinner spinner-bounce-top"></div>
+                </div>
+                <div className="row" style={{ ...styles.flexCenter, ...styles.row }}>
+                    <div className="spinner spinner-bounce-middle"></div>
+                </div>
+                <div className="row" style={{ ...styles.flexCenter, ...styles.row }}>
+                    <div className="spinner spinner-blink"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
