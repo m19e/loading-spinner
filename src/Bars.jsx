259097872f55ace27818ab543e8c09d71a61c671
m19e <nexus.me7290@gmail.com>
2020-12-19 07:31:28 +0900

    Fix vertical typing loader

--- src/Bars.jsx
+++ src/Bars.jsx
@@ -1,4 +1,5 @@
 import "./Bars.css";
+import "./Loader.css";
 
 const styles = {
     flexCenter: {
@@ -14,9 +15,9 @@ const styles = {
 
 export default function Bars() {
     return (
-        <div style={{ width: "100vw", height: "100vh" }}>
-            <div className="spinners" style={{ display: "flex", flex: 1, flexDirection: "column", height: "100%" }}>
-                {/* <div className="row" style={{ ...styles.flexCenter, ...styles.row }}>
+        <div style={{ width: "100vw", height: "100vh", backgroundColor: "#f9f9fa" }}>
+            {/* <div className="spinners" style={{ display: "flex", flex: 1, flexDirection: "column", height: "100%" }}>
+                <div className="row" style={{ ...styles.flexCenter, ...styles.row }}>
                     <div className="spinner spinner-bounce-bottom"></div>
                 </div>
                 <div className="row" style={{ ...styles.flexCenter, ...styles.row }}>
@@ -27,12 +28,26 @@ export default function Bars() {
                 </div>
                 <div className="row" style={{ ...styles.flexCenter, ...styles.row }}>
                     <div className="spinner spinner-blink"></div>
-                </div> */}
+                </div>
+
                 <div className="row" style={{ ...styles.flexCenter, ...styles.row }}>
                     <div className="spinner spinner-bounce-text"></div>
                 </div>
                 <div className="row" style={{ ...styles.flexCenter, ...styles.row }}>
                     <div className="spinner text-spinner spinner-text-fast"></div>
+                </div>
+            </div> */}
+            <div style={{ flex: 1, height: "100%", ...styles.flexCenter }}>
+                <div style={{ width: "160px", height: "160px", ...styles.flexCenter }}>
+                    <div style={{ display: "flex", flexDirection: "column", height: "80px" }}>
+                        <div className="loader-before"></div>
+                        <div style={{ width: "16px", height: "16px" }}></div>
+                    </div>
+                    <div className="loader"></div>
+                    <div style={{ display: "flex", flexDirection: "column", height: "80px" }}>
+                        <div style={{ width: "16px", height: "16px" }}></div>
+                        <div className="loader-after"></div>
+                    </div>
                 </div>
             </div>
         </div>
